66cdd9b468b9d95325f41c8fc5f2bd0ed84fecca
Michael Kutka <michael.kutka@gmail.com>
2023-04-05 07:24:43 -0400

    updates to restconf-get.py

--- IOS-XE/restconf-get.py
+++ IOS-XE/restconf-get.py
@@ -3,7 +3,7 @@ import json
 from pprint import pprint
 
 # set up connection paramters in a dictionary
-router = {"ip": "ios-xe-mgmt.cisco.com", "port": "9443", "user": "root", "pass": "D_Vay!_10&"}
+router = {"ip": "sandbox-iosxe-latest-1.cisco.com", "port": "443", "user": "admin", "pass": "C1sco12345"}
 
 # set REST API headers
 headers = {"Accept": "application/yang-data+json", 
@@ -15,8 +15,8 @@ response = requests.get(url, headers=headers, auth=(router["user"], router["pass
 
 api_data = response.json()
 print("/" * 50)
-pprint(api_data["Cisco-IOS-XE-interfaces-oper:interface"]["description"])
+pprint(api_data["Cisco-IOS-XE-interfaces-oper:interface"][0]["description"])
 print("/" * 50)
-if api_data["Cisco-IOS-XE-interfaces-oper:interface"]["admin-status"] == "if-state-up":
+if api_data["Cisco-IOS-XE-interfaces-oper:interface"][0]["admin-status"] == "if-state-up":
     print("Interface is up")
 
